f390bbc13c0c3f79c988ae465ecfe8cc7620606f
misawa-tasuku <plusnine339@gmail.com>
2021-09-14 07:44:22 +0900

    update-files

--- React/react-hooks-todo-app/src/components/App.jsx
+++ React/react-hooks-todo-app/src/components/App.jsx
@@ -1,25 +1,67 @@
 import React, {useReducer, useState} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoForm from './TodoForm'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import reducer from '../reducers'
-import Todos from './Todos'
-
+import Todo from './Todo'
 
 const App = () =>  {
   const [state, dispatch] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
 
-  // const [incompleteTodos, setIncompleteTodos] = useState([])
+  const addTodo = e => {
+    e.preventDefault()//画面のリロード防ぐ
+    dispatch({
+      type: 'CREATE_TODO',
+      title,
+      body,
+    })
+    setTitle('')
+    setBody('')
+  }
 
-  // const [doingTodos, setDoingTodos] = useState([])
-  // const [completeTodos, setCompleteTodos] = useState([])
+
+
+  const deleteAllTodo = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのTODOを本当に削除してもよろしいですか?')
+    dispatch( {type: 'DELETE_ALL_TODOS'} )
+  }
+
+  const unCreatable = title === '' || body === ''
 
   return (
     // JSXだとclassName
     <div className="container-fluid">
-      <TodoForm state={state} dispatch={dispatch} />
-      
-     
-      <Todos state={state} dispatch={dispatch} />
+      <h4>TODO作成フォーム</h4> 
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">タイトル</label>
+          <input type="form-control" id="formEventTitle" value={title} 
+          onChange={e => setTitle(e.target.value)} />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventBody">内容</label>
+          <textarea type="form-control" id="formEventBody" value={body}
+          onChange={e => setBody(e.target.value)} />
+        </div>
+        <button className="btn btn-primary" onClick={addTodo} disabled={unCreatable}>TODO作成</button>
+        <button className="btn btn-danger" onClick={deleteAllTodo} disabled={state.length === 0}>全てのTODOを一括削除</button>
+      </form>
+      <h4>TODO一覧</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>タイトル</th>
+            <th>内容</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            state.map((todo, index)=> (<Todo key={index} todo={todo} dispatch={dispatch} />))
+          }
+        </tbody>
+      </table>
     </div>
     
   )
